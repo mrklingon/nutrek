fc06be1f1f638841783a4ffa5c1b6e1fa1ca0588
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-12-17 18:06:04 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+let StationPan = images.createBigImage(`
+    # . . . . . . # . .
+    . . . # . . # # # .
+    . . . . . . # # # .
+    . . # . . . . # . .
+    . . . . . . . # . .
+    `)
+let Station = images.createImage(`
+    . . # . .
+    . # # # .
+    . # # # .
+    . . # . .
+    . . # . .
+    `)
+StationPan.scrollImage(1, 200)
+basic.pause(2000)
+let Starbase = game.createSprite(2, 1)
+basic.forever(function () {
+	
+})
